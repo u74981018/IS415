a8bd2a597c4909969180083a8999fc248beea9b2
Lucas-Vial <viallucas03@gmail.com>
2024-11-11 11:23:16 +0800

    inclass - week 13

--- take-home/shiny/takehome3/app.R
+++ take-home/shiny/takehome3/app.R
@@ -1,33 +1,32 @@
-library(shiny)
-pacman::p_load(sfdep, tidyverse, sf, tmap, lubridate, spatstat)
+# Packages 
+pacman::p_load(sfdep, tidyverse, sf, tmap, lubridate, spatstat, dplyr, ggplot2, viridis, magick, raster, sparr, rmapshaper)
 
-# Load data
-adm1 <- readRDS("data/papua_adm1.rds")
-adm2 <- readRDS("data/papua_adm2.rds")
-ps <- readRDS("data/points_study.rds")
+# Aspatial Data
+ps <- readRDS("data/3data/aspatial/points_study.rds")
 
-# Convert event_date to Unix timestamps
+# Geospatial Data 
+adm1 <- readRDS("data/adm1_window.rds")
+
 ps <- ps %>%
-  mutate(event_date_unix = as.numeric(dmy(event_date)))
+  mutate(event_date = dmy(event_date))
 
-# UI
 ui <- fluidPage(
-  titlePanel("Kernel Density Estimation and Spatial Analysis"),
+  titlePanel("PART 3 - Spatio-Temporal KDE Analysis"),
   sidebarLayout(
     sidebarPanel(
-      dateRangeInput("date_range", "Select Date Range:",
-                     start = as.Date("2020-01-01"),
-                     end = as.Date("2020-12-31")),
-      selectInput("bandwidth_method", "Bandwidth Selection Method:",
-                  choices = c("bw.diggle", "bw.CvL", "bw.scott", "bw.ppl", "adaptive")),
-      selectInput("smoothing_kernel", "Smoothing Kernel Method:",
-                  choices = c("Gaussian", "Epanechnikov", "Quartic", "Disc")),
-      selectInput("second_order_analysis", "Second Order Analysis Method:",
-                  choices = c("G-function", "F-function", "K-function", "L-function")),
-      numericInput("num_simulations", "Number of Simulations:", value = 99, min = 1),
-      selectInput("edge_correction", "Edge Correction:",
-                  choices = c("None", "Translational", "Border")),
-      actionButton("run_analysis", "Run Analysis")
+      selectInput("years", "Time Range:",
+                  choices = c("All", "2015", "2016", "2017", "2018", "2019", 
+                              "2020", "2021", "2022", "2023", "2024"),
+                  multiple = TRUE),
+      selectInput("study_area", "Study Area:",
+                  choices = c("All", "Papua", "Papua Barat", "Papua Barat Daya", 
+                              "Papua Pegunungan", "Papua Selatan", "Papua Tengah"),
+                  multiple = TRUE),
+      radioButtons("qorm", "Split Data By:",
+                   choices = c("Quarterly", "Monthly")),
+      actionButton("start", "Visualising the Data"),
+      br(),
+      actionButton("startKDE", "Generate STKDE Animation")
     ),
     mainPanel(
       plotOutput("mapPlot", width = "100%", height = 400)
@@ -37,55 +36,9 @@ ui <- fluidPage(
 
 # Server
 server <- function(input, output) {
-  observeEvent(input$run_analysis, {
-    print("Run Analysis Button Clicked")  # Debugging
-    
-    # Convert date range input to Unix timestamps
-    date_range_unix <- as.numeric(as.Date(input$date_range))
-    print(paste("Date Range:", date_range_unix[1], "to", date_range_unix[2]))  # Debugging
-    
-    # Filter `ps` dataset using the converted Unix timestamps
-    filtered_ps <- ps %>%
-      filter(event_date_unix >= date_range_unix[1] & event_date_unix <= date_range_unix[2])
-    
-    # Check if filtered_ps is not empty
-    if (nrow(filtered_ps) == 0) {
-      output$mapPlot <- renderPlot({ plot.new() })  # If no data, create a blank plot
-      output$secondOrderPlot <- renderPlot({ plot.new() })
-      return()
-    }
-    # Transform to target CRS
-    target_crs <- 23883
-    adm1_proj <- st_transform(adm1, crs = target_crs)
-    ps_proj <- st_make_valid(st_transform(filtered_ps, crs = target_crs))  # Ensure valid geometries
-    
-    # Convert filtered points to a spatial point pattern
-    ps_ppp <- as.ppp(st_geometry(ps_proj))  # Use as.ppp directly
-    adm1_owin <- as.owin(st_geometry(adm1_proj))
-    ps_ppp_adm1 <- ps_ppp[adm1_owin]
-    
-    # Select bandwidth method
-    bandwidth_method <- switch(input$bandwidth_method,
-                               "bw.diggle" = bw.diggle,
-                               "bw.CvL" = bw.CvL,
-                               "bw.scott" = bw.scott,
-                               "bw.ppl" = bw.ppl,
-                               "adaptive" = function(x) bw.adapt(x, 0.05))  # Example for adaptive
-    
-    # Select kernel type
-    kernel_type <- switch(input$smoothing_kernel,
-                          "Gaussian" = "gaussian",
-                          "Epanechnikov" = "epanechnikov",
-                          "Quartic" = "quartic",
-                          "Disc" = "disc")
-    
-    # Compute Kernel Density Estimation
-    kde <- density(ps_ppp_adm1, sigma = bandwidth_method, edge = TRUE, kernel = kernel_type)
-    
-    # Plot KDE
-    output$mapPlot <- renderPlot({
-      plot(kde, main = paste("Kernel Density Estimation (Method:", input$bandwidth_method, ")"))
-    })
+  # Server logic goes here
+  observeEvent(input$start, {
+
   })
 }
 
